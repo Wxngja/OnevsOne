07ac5a0b235772dc8d8f00704833d6a53325b563
LCraftPE <LCraftPE@users.noreply.github.com>
2017-03-07 22:19:38 +0100

    Update Main.php

--- src/OnevsOne/Main.php
+++ src/OnevsOne/Main.php
@@ -65,8 +65,9 @@ class Main extends PluginBase implements Listener{
   
   public function launchDuel($arena){
     if(count($this->players[$arena]) == 2){
-      $this->game[$arena][$this->players[$arena][0]->getName()] = $this->players[$arena][1];
-      $this->game[$arena][$this->players[$arena][1]->getName()] = $this->players[$arena][0];
+      $array = array_keys($this->players[$arena]);
+      $this->game[$arena][$this->players[$arena][$array[0]]->getName()] = $this->players[$arena][$array[1]];
+      $this->game[$arena][$this->players[$arena][$array[1]]->getName()] = $this->players[$arena][$array[0]];
       $this->players[$arena][1]->sendMessage("§6§l1vs1 §r§7Vous affrontez §f".$this->players[$arena][0]->getName());
       $this->players[$arena][0]->sendMessage("§6§l1vs1 §r§7Vous affrontez §f".$this->players[$arena][1]->getName());
       $this->prepareToFight($this->players[$arena][0]);
